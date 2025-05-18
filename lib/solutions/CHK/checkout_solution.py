0c829ffd2b072aead94616b01f1d2ee1daf1b543
sbx_user1051 <sbx_user1051@169.254.12.113>
2025-05-18 16:11:24 +0000

    Sun May 18 16:11:24 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,15 @@ class CheckoutSolution:
 
     # skus = unicode string
     def checkout(self, skus: str) -> int:
+        price_table = {
+            "A": {"price": 50, "offer": (3,130)},
+            "B": {"price": 30, "offer": (2,45)},
+            "C": {"price": 20, "offer": None},
+            "D": {"price": 15, "offer": None}
+        }
+
+        total = 0
+        from collections import Counter
+        counts = Counter(skus)
+
         
